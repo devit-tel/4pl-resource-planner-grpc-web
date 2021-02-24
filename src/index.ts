bf3306c1b878ade6817fc67fa78ec5cf2ebfb800
NV4RE <tosbodesz@gmail.com>
2021-02-25 00:38:32 +0700

    Add client

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { Candidates } from "./protos/coordinator/coordinator_pb";
+import * as grpc from "@grpc/grpc-js";
+import * as coordinatorService from "./protos/coordinator/coordinator_pb_service";
 
-export const Can = new Candidates();
+export const newCoordinatorClient = (host: string) => {
+  return new coordinatorService.CoordinatorClient(
+    host,
+    grpc.credentials.createInsecure()
+  );
+};
